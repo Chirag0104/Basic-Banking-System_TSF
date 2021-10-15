603774eb763fc314e2d90779c8f09a32b62925b4
Chirag0104 <chiraggoyal0104@gmail.com>
2021-10-16 00:29:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-  <?php
-      // $title = 'Index';
+<?php
       require_once 'includes/navbar.php';
       require_once 'db/conn.php';
-  ?>
+?>
+
   <div id="home">
-
     <h1 class="primary center"> Welcome To The Best Bank </h1>
-
-    <div id="services">
-
-      <div class="box">
-
-        <img src="images/newuser.png" alt="Create a new User" />
-
-        <h2 id="head">
-          <a href="newuser.php">
+      <div id="services">
+        <div class="box">
+          <img src="images/newuser.png" alt="Create a new User" />
+            <h2 id="head">
+            <a href="newuser.php">
             <button class="btn center">
               New User
             </button>
@@ -24,21 +19,21 @@
 
       <div class="box">
         <img src="images/Transaction.png" alt="Make A Transaction">
-        <h2 id="head"><a href="transfer-money.php">
+          <h2 id="head"><a href="transfer-money.php">
             <button class="btn center">
               Make A Transaction
             </button></a>
-        </h2>
+          </h2>
       </div>
 
       <div class="box">
         <img src="images/make-transaction.png" alt="Transaction History" />
-        <h2 id="head">
-          <a href="transferdetails.php">
-            <button class="btn center">
-              Transaction History
-            </button>
-          </a>
+          <h2 id="head">
+            <a href="transferdetails.php">
+              <button class="btn center">
+                Transaction History
+              </button>
+            </a>
         </h2>
       </div>
 
@@ -46,10 +41,8 @@
   </div>
 
   <?php
-      // $title = 'Index';
       require_once 'includes/footer.php'
   ?>
 
 </body>
-
 </html>
