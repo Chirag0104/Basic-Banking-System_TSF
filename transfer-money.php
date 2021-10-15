b864a7e0ba7ee17a9b5957a5641fb4adae266c00
Chirag0104 <chiraggoyal0104@gmail.com>
2021-10-16 00:29:07 +0530

    Update transfer-money.php

--- transfer-money.php
+++ transfer-money.php
@@ -1,5 +1,4 @@
 <?php
-      // $title = 'Index';
       require_once 'includes/navbar.php';
       include 'db/conn.php';
 
@@ -15,30 +14,29 @@
       $reciver = $crud->get_data();
 
       
-  ?>
+?>
 
     <div id="home-transfermoney">
         <div class="container-3">
             <div class="center">
                 <h1>Transfer Money</h1></div>
                 <form action="transferdetails.php"  method="POST">
-            <div class="label-1">
-                <label for="sender-detail"><h4>Select Sender Details</h4> </label>
+                    <div class="label-1">
+                    <label for="sender-detail"><h4>Select Sender Details</h4> </label>
 
-                <select name="SName" id="sender" required>
-                <option value="" >Select</option>
-                <?php 
-                    while($s=$results->fetch(PDO::FETCH_ASSOC)){ ?>
-                    <option value="<?php echo $s['name'] ?>" ><?php  
-                     echo 'Name - '.$s['name'].'      ';
-                     echo 'Mail - '.$s['email'].'      '; 
-                     echo 'Balance - '.$s['balance'].'      ';
-                     
-                       ?></option>
-                <?php }
-
-                ?>
-                </select>
+                    <select name="SName" id="sender" required>
+                        <option value="" >Select</option>
+                            <?php 
+                                while($s=$results->fetch(PDO::FETCH_ASSOC)){ ?>
+                                <option value="<?php echo $s['name'] ?>" ><?php  
+                                echo 'Name - '.$s['name'].'      ';
+                                echo 'Mail - '.$s['email'].'      '; 
+                                echo 'Balance - '.$s['balance'].'      ';
+                                ?>
+                        </option>
+                            <?php }
+                            ?>
+                    </select>
             </div>
 
             <div class="label-2">
@@ -53,30 +51,31 @@
                 <label for="reciever-detail"><h4>Select Reciever Details </h4></label>
 
                 <select name="RName" id="reciever" required >
-                <option value="">Select</option>
-                <?php 
-                    while($s=$reciver->fetch(PDO::FETCH_ASSOC)){ ?>
-                    <option value="<?php echo $s['name'] ?>" ><?php  
-                    echo $s['name']; 
-                    echo $s['email']; 
-                      ?></option>
-                <?php }
+                    <option value="">Select</option>
 
-                ?>
+                        <?php 
+                            while($s=$reciver->fetch(PDO::FETCH_ASSOC)){ ?>
+                            <option value="<?php echo $s['name'] ?>" ><?php  
+                            echo $s['name']; 
+                            echo $s['email']; 
+                            ?>
+                    </option>
+                        <?php }
+                        ?>
+
                 </select>
             </div>
             <div class="btn-transfer">
                 
                 <input type="submit" class="btn-4 transfer-page " value="submit" name="submit">
             </div>
+
         </form>
         </div>
-        <?php
-      // $title = 'Index';
-      require_once 'includes/footer.php'
-      ?>
-    </div>
+<?php
+    require_once 'includes/footer.php'
+?>
 
+</div>
 </body>
-
 </html>
