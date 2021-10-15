91ac784f66812f7d4f71dff374ba06b8deba84a7
Chirag0104 <chiraggoyal0104@gmail.com>
2021-10-16 00:29:05 +0530

    Update newuser.php

--- newuser.php
+++ newuser.php
@@ -2,34 +2,33 @@
       // $title = 'Index';
       require_once 'includes/navbar.php';
       require_once 'db/conn.php';
-  ?>
-  <div id="home-1">
-    
+?>
+  <div id="home-1"> 
     <div class="container-2">
       <h1>Create a User</h1>
       <img src="images/user.png" alt="">
-      <form action="transfer-money.php" method="POST">
-        <div class="form-group">
-          <input type="text" name="name" required placeholder="Enter Your Name" >
-        </div>
-        <div class="form-group">
-          <input type="email" name="mail" placeholder="Enter Your Mail ID" required>
-        </div>
-        <div class="form-group">
-          <input type="number" name="balance" placeholder="Enter Your Balance" required>
-          <!-- <input type="number" value="1" min="1" max="8" style="font-size: 55px;"> -->
-        </div>
-        <input type="submit" name='submit' class="btn-1 " value="Submit">
-        
-      </form>
+
+        <!-- Input Form for new user       -->
+
+        <form action="transfer-money.php" method="POST">
+          <div class="form-group">
+            <input type="text" name="name" required placeholder="Enter Your Name" >
+          </div>
+          <div class="form-group">
+            <input type="email" name="mail" placeholder="Enter Your Mail ID" required>
+          </div>
+          <div class="form-group">
+            <input type="number" name="balance" placeholder="Enter Your Balance" required>
+          </div>
+          <input type="submit" name='submit' class="btn-1 " value="Submit">
+        </form>
+
     </div>
   </div>
 
-  <?php
-      // $title = 'Index';
+<?php
       require_once 'includes/footer.php'
-  ?>
+?>
 
 </body>
-
 </html>
